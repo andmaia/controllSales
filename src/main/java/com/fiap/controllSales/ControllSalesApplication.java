51629769697312b7ff20275df722d06b01863bec
andmaia <andrewrian198@gmail.com>
2023-10-08 22:09:11 -0300

    Initial commit

---
+++ src/main/java/com/fiap/controllSales/ControllSalesApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.controllSales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllSalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/controllSales/ControllSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.controllSales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
